51e2991b9fd79a5c9d36b4c7a292bdf7c983b114
Yahaya Azeez <azconcept2016@gmail.com>
2023-08-08 14:52:53 +0100

    uncomment some code

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ function greetAzeez(greetFn) {
 }
 
 greetAzeez(greet)
-// const data = require('./data.json')
-// const path = require("node:path")
-// const path = require("path")
+const data = require('./data.json')
+const path = require("node:path")
+const path = require("path")
 
-// console.log(path.basename(__filename))
-// console.log(path.basename(__dirname))
+console.log(path.basename(__filename))
+console.log(path.basename(__dirname))
 
-// console.log(path.extname(__filename))
-// console.log(path.extname(__dirname))
+console.log(path.extname(__filename))
+console.log(path.extname(__dirname))
 
-// console.log(path.parse(__filename))
+console.log(path.parse(__filename))
 
-// console.log(path.format(path.parse(__filename)))
+console.log(path.format(path.parse(__filename)))
 
-// console.log(path.isAbsolute(__filename))
-// console.log(path.isAbsolute("./data.json"))
+console.log(path.isAbsolute(__filename))
+console.log(path.isAbsolute("./data.json"))
 
-// console.log(data.name)
+console.log(data.name)
 
 // console.log(path.join("folder1", "folder2", "index.html"))
 // console.log(path.join("/folder1", "folder2", "index.html"))
