a5b5773c2907e84ab913d3f3a3569ddb88fd9bf3
Yahaya Azeez <azconcept2016@gmail.com>
2023-08-09 19:01:32 +0100

    just add :poop: code

--- emmitter.js
+++ emmitter.js
@@ -6,4 +6,10 @@ emitter.on('order-pizza', (size, topping) => {
     console.log(`Order received! Baking a ${size} pizza with ${topping}`)
 })
 
+emitter.on('order-pizza', (size) => {
+    if (size === 'large') {
+        console.log(`Order received! Baking a ${size} pizza with ${topping}`)
+    }
+})
+
 emitter.emit('order-pizza', 'large', 'mushroom')
