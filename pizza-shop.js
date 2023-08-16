2739f412c5d08c62abf4e9fcb86f66851afe0be9
Yahaya Azeez <azconcept2016@gmail.com>
2023-08-16 11:50:06 +0100

    inheriting from event module :children_crossing:

---
+++ pizza-shop.js
@@ -0,0 +1,20 @@
+const eventEmitter = require('node:events')
+const { setUncaughtExceptionCaptureCallback } = require('node:process')
+
+class pizzaShop extends eventEmitter{
+    constructor() {
+        super()
+        this.orderNumber = 0
+    }
+
+    order(size, topping) {
+        this.orderNumber++
+        this.emit('order', size, topping)
+    }
+
+    displayOrderNumber() {
+        console.log(`Current order number: ${this.orderNumber}`)
+    }
+}
+
+module.exports = pizzaShop
