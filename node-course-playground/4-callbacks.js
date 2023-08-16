45162cb212bee9388cfa482d54ee49921cb4e5a4
Yahaya Azeez <azconcept2016@gmail.com>
2023-08-16 10:55:29 +0100

    add new directory playground :tada:

--- node-course-playground/4-callbacks.js
+++ node-course-playground/4-callbacks.js
@@ -23,9 +23,9 @@
 //     console.log(data)
 // })
 
-//
+
 // Goal: Mess around with the callback pattern
-//
+
 // 1. Define an add function that accepts the correct arguments
 // 2. Use setTimeout to simulate a 2 second delay
 // 3. After 2 seconds are up, call the callback function with the sum
